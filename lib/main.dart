4af1fda79fab8ca7b2db75eb93f3aa375c70b6ee
AbduLlAh72727272 <maw112266@gmail.com>
2024-08-15 19:51:09 +0500

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 import 'app/routes/app_pages.dart';
 
 void main() async {
-    WidgetsFlutterBinding.ensureInitialized();
-   await GetStorage.init();
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // if (WebView.platform == null) {
+  //   WebView.platform = SurfaceAndroidWebView();
+  // }
+  // Initialize GetStorage
+  await GetStorage.init();
+
   runApp(
     ScreenUtilInit(
       designSize: const Size(360, 690),
@@ -19,7 +25,6 @@ void main() async {
           title: "Application",
           debugShowCheckedModeBanner: false,
           initialRoute: AppPages.SPLASH_SCREEN,
-        //  initialRoute: AppPages.BOTTOM_NAV_BAR,
           getPages: AppPages.routes,
         );
       },
